0ba9ccf26a1aaacd03e3ae10206dbb73978a3c65
Franklim Porfirio <81085657+FranklimSilva@users.noreply.github.com>
2022-11-28 18:45:58 -0300

    Add files via upload

---
+++ Classes/ComboHelpers.cs
@@ -0,0 +1,98 @@
+﻿using ECommerceProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceProject.Classes
+{
+    public class ComboHelpers : IDisposable
+    {
+        private static EcommerceContext db = new EcommerceContext();
+
+        public static List<Departaments> GetDepartaments()
+        {
+            var dep = db.Departaments.ToList();
+            dep.Add(new Departaments
+            {
+                DepartamentsId = 0,
+                Name = "[Selecione um Departamento]"
+            });
+
+           return dep = dep.OrderBy(d => d.Name).ToList();
+        }
+        public static List<City> GetCities()
+        {
+            var city = db.Cities.ToList();
+            city.Add(new City
+            {
+                CityId = 0,
+                Name = "[Selecione uma Cidade]"
+            });
+
+            return city = city.OrderBy(d => d.Name).ToList();
+        }
+
+        public static List<Company> GetCompany()
+        {
+            var company = db.Companies.ToList();
+            company.Add(new Company
+            {
+                CompanyId = 0,
+                Name = "[Selecione uma Companhia]"
+            });
+
+            return company = company.OrderBy(c => c.Name).ToList();
+        }
+        public static List<Category> GetCategory(int companyId)
+        {
+            var cat = db.Categories.Where(c => c.CompanyId == companyId).ToList();
+            cat.Add(new Category
+            {
+                CategoryId = 0,
+                Description = "[Selecione uma Categoria]"
+            });
+
+            return cat = cat.OrderBy(c => c.Description).ToList();
+        }
+
+        public static List<Customer> GetCustomer( int companyId)
+        {
+            var customer = db.Customers.Where(c=> c.CompanyId == companyId).ToList();
+            customer.Add(new Customer
+            {
+                CustumerId = 0,
+                FirstName = "[Selecione um Cliente]"
+            });
+
+            return customer = customer.OrderBy(c => c.FirstName).ThenBy(c=>c.LastName).ToList();
+        }
+
+        public static List<Tax> GetTaxes(int companyId)
+        {
+            var tax = db.Taxes.Where(c => c.CompanyId == companyId).ToList();
+            tax.Add(new Tax
+            {
+                TaxId = 0,
+                Description = "[Selecione uma Taxa]"
+            });
+
+            return tax = tax.OrderBy(c => c.Description).ToList();
+        }
+        public static List<Product> GetProduct(int companyId)
+        {
+            var product = db.Products.Where(c=> c.CompanyId == companyId).ToList();
+            product.Add(new Product
+            {
+                ProductId = 0,
+                Description = "[Selecione um Produto]"
+            });
+
+            return product = product.OrderBy(c => c.Description).ToList();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ Classes/UserHelper.cs
@@ -0,0 +1,141 @@
+﻿using ECommerceProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace ECommerceProject.Classes
+{
+    public class UserHelper : IDisposable
+    {
+        private static ApplicationDbContext userContext = new ApplicationDbContext();
+        private static EcommerceContext db = new EcommerceContext();
+        
+        //Deletar Usuario
+        public static bool DeleteUser(string userName)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(userName);
+
+            if(userASP == null)
+            {
+                return false;
+            }
+            var response = userManager.Delete(userASP);
+            return response.Succeeded;
+        }
+
+        //Atualizar Usuario
+        public static bool UpdateUserName(string currentUserName, string newUserName)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(currentUserName);
+
+            if (userASP == null)
+            {
+                return false;
+            }
+
+            userASP.UserName = newUserName;
+            userASP.Email = newUserName;
+
+            var response = userManager.Update(userASP);
+            return response.Succeeded;
+
+
+        }
+
+
+        public static void CheckRole(string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(userContext));
+
+            //check to see if Role exists, if not create it 
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole(roleName));
+            }
+
+        }
+
+        public static void CheckSuperUser()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var email = WebConfigurationManager.AppSettings["AdminUser"];
+            var password = WebConfigurationManager.AppSettings["AdminPassWord"];
+            var userASP = userManager.FindByName(email);
+
+            if(userASP == null)
+            {
+                CreateUserASP(email, "Admin", password);
+                return;
+            }
+
+            userManager.AddToRole(userASP.Id, "Admin");
+        }
+
+        public static void CreateUserASP(string email, string roleName)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+
+            var userASP = new ApplicationUser
+            {
+                Email = email,
+                UserName = email
+            };
+            userManager.Create(userASP, email);
+            userManager.AddToRole(userASP.Id, roleName);
+        }
+        public static void CreateUserASP(string email, string roleName, string password)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+
+            var userASP = new ApplicationUser
+            {
+                Email = email,
+                UserName = email
+            };
+            userManager.Create(userASP, password);
+            userManager.AddToRole(userASP.Id, roleName);
+        }
+        public static async Task PasswordRecovery(string email)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(email);
+            if(userASP == null)
+            {
+                return;
+            }
+            var user = db.Users.Where(tp => tp.UserName == email).FirstOrDefault();
+            if(user == null)
+            {
+                return;
+            }
+
+            var random = new Random();
+            var newPassword = string.Format("{0}{1}{2:04}*",
+                user.FirstName.Trim().ToUpper().Substring(0, 1),
+                user.LastName.Trim().ToLower(),
+                random.Next(10000));
+            userManager.RemovePassword(userASP.Id);
+            userManager.AddPassword(userASP.Id, newPassword);
+
+            var subject = "A senha foi modificada";
+            var body = string.Format(@"
+                    <h1>A senha foi modificada<h1>
+                    <p>Sua nova senha é <strong>{0}</p>
+                    <p>Sua senha foi alterada com sucesso!</p>",
+                    newPassword);
+            await EmailHelper.SendEmail(email,subject,body);
+
+        }
+
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
